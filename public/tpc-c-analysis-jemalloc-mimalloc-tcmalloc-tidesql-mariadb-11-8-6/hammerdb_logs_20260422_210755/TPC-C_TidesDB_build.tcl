3ddbcb65af1eca189c17f62700f15c1fdeaa8018
Alex Gaetano Padula <me@alexpadula.com>
2026-04-22 18:10:43 -0400

    allocator analysis

---
+++ public/tpc-c-analysis-jemalloc-mimalloc-tcmalloc-tidesql-mariadb-11-8-6/hammerdb_logs_20260422_210755/TPC-C_TidesDB_build.tcl
@@ -0,0 +1,16 @@
+puts "SETTING CONFIGURATION"
+dbset db maria
+dbset bm TPC-C
+diset connection maria_host localhost
+diset connection maria_port 3306
+diset connection maria_socket /tmp/mariadb.sock
+diset tpcc maria_count_ware 40
+diset tpcc maria_num_vu 8
+diset tpcc maria_user hammerdb
+diset tpcc maria_pass hammerdb123
+diset tpcc maria_dbase tpcc
+diset tpcc maria_storage_engine [string tolower TidesDB]
+diset tpcc maria_partition false
+puts "SCHEMA BUILD STARTED"
+buildschema
+puts "SCHEMA BUILD COMPLETED"
